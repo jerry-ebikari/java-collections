c88953323888391cae1512f7e0b387a19bcd7e5d
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-03-02 15:07:27 +0100

    Sort array lists in a specific order

--- src/arrayList/SortArrayList.java
+++ src/arrayList/SortArrayList.java
@@ -1,17 +1,29 @@
 package arrayList;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortArrayList {
     static <T extends Comparable<T>> void sort(ArrayList<T> arrayList) {
         if (arrayList == null) return;
-        arrayList.sort(Comparable::compareTo);
+        arrayList.sort(Comparator.naturalOrder());
+    }
+
+    static <T extends Comparable<T>> void sort(ArrayList<T> arrayList, String order) {
+        if (arrayList == null) return;
+        if (order == null || !order.equals("desc")) {
+            arrayList.sort(Comparator.naturalOrder());
+        } else {
+            arrayList.sort(Comparator.reverseOrder());
+        }
     }
 
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>(List.of("Jerry", "Ebikari", "Needam", "James", "Bond"));
         sort(names);
+        System.out.println(names);
+        sort(names, "desc");
         System.out.println(names);
     }
 }
